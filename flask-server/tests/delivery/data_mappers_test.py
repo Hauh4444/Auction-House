47d43660c2c670a1a6bd09b97196494029797bfb
Hauh4444 <150862142+Hauh4444@users.noreply.github.com>
2025-03-21 13:50:18 -0400

    fixed some of the tests

--- flask-server/tests/delivery/data_mappers_test.py
+++ flask-server/tests/delivery/data_mappers_test.py
@@ -15,32 +15,30 @@ def test_get_all_deliveries(mock_db_session):
     mock_cursor = mock_db_session.cursor.return_value
     mock_cursor.fetchall.return_value = [
         {
-            "order_id": 1,
+            "order_item_id": 1,
             "user_id": 1,
             "address": "123 Street",
             "city": "Indiana",
             "state": "Pennsylvania",
-            "postal_code": "15701",
             "country": "United States",
             "delivery_status": "delivered"
         },
         {
-            "order_id": 2,
+            "order_item_id": 2,
             "user_id": 1,
             "address": "123 Street",
             "city": "Indiana",
             "state": "Pennsylvania",
-            "postal_code": "15701",
             "country": "United States",
             "delivery_status": "delivered"
         }
     ]
 
-    deliveries = DeliveryMapper.get_all_deliveries(db_session=mock_db_session)
+    deliveries = DeliveryMapper.get_all_deliveries(user_id=1, db_session=mock_db_session)
 
     assert len(deliveries) == 2
-    assert deliveries[0]["user_id"] == 1
-    assert deliveries[1]["user_id"] == 1
+    assert deliveries[0]["order_item_id"] == 1
+    assert deliveries[1]["order_item_id"] == 2
     assert isinstance(deliveries[0]["address"], str)
     assert isinstance(deliveries[1]["address"], str)
 
@@ -48,19 +46,18 @@ def test_get_all_deliveries(mock_db_session):
 def test_get_delivery_by_id(mock_db_session):
     mock_cursor = mock_db_session.cursor.return_value
     mock_cursor.fetchone.return_value = {
-        "order_id": 1,
+        "order_item_id": 1,
         "user_id": 1,
         "address": "123 Street",
         "city": "Indiana",
         "state": "Pennsylvania",
-        "postal_code": "15701",
         "country": "United States",
         "delivery_status": "delivered"
     }
 
     delivery = DeliveryMapper.get_delivery_by_id(delivery_id=1, db_session=mock_db_session)
 
-    assert delivery["order_id"] == 1
+    assert delivery["order_item_id"] == 1
     assert delivery["user_id"] == 1
     assert isinstance(delivery["address"], str)
 
@@ -69,12 +66,11 @@ def test_create_delivery(mock_db_session):
     mock_cursor = mock_db_session.cursor.return_value
     mock_cursor.lastrowid = 3
     data = {
-        "order_id": 1,
+        "order_item_id": 1,
         "user_id": 1,
         "address": "123 Street",
         "city": "Indiana",
         "state": "Pennsylvania",
-        "postal_code": "15701",
         "country": "United States",
         "delivery_status": "delivered"
     }
@@ -97,12 +93,11 @@ def test_create_delivery_missing_fields(mock_db_session):
     mock_cursor = mock_db_session.cursor.return_value
     mock_cursor.lastrowid = 3
     data = {
-        "order_id": 1,
+        "order_item_id": 1,
         "user_id": 1,
         "address": "123 Street",
         "city": "Indiana",
         "state": "Pennsylvania",
-        "postal_code": "15701",
         "country": "United States",
         "delivery_status": "delivered"
     }
@@ -118,12 +113,11 @@ def test_create_delivery_invalid_data_type(mock_db_session):
     mock_cursor = mock_db_session.cursor.return_value
     mock_cursor.lastrowid = 3
     data = {
-        "order_id": 1,
+        "order_item_id": 1,
         "user_id": 1,
         "address": "123 Street",
         "city": "Indiana",
         "state": "Pennsylvania",
-        "postal_code": "15701",
         "country": "United States",
         "delivery_status": "delivered"
     }
@@ -156,12 +150,11 @@ def test_create_delivery_db_failure(mock_db_session):
     mock_cursor.execute.side_effect = Exception("Database error")
 
     data = {
-        "order_id": 1,
+        "order_item_id": 1,
         "user_id": 1,
         "address": "123 Street",
         "city": "Indiana",
         "state": "Pennsylvania",
-        "postal_code": "15701",
         "country": "United States",
         "delivery_status": "delivered"
     }
