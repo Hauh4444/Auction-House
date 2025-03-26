9198d8672a8926ccf5dc4f7be7c55e09ecdd93ca
Hudson727 <dhgeid@gmail.com>
2025-03-26 15:08:41 -0400

    Update base.py

--- flask-server/app/config/base.py
+++ flask-server/app/config/base.py
@@ -1,6 +1,8 @@
 import os
 from datetime import timedelta
-
+from flask import Flask
+from flask_mail import Mail
+from flask_notifications import Notifications
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
@@ -15,3 +17,15 @@ class Config:
     CORS_ORIGINS = "http://localhost:5173"
     LIMITER_STORAGE_URI = os.getenv("memory://")
     DEFAULT_RATE_LIMITS = ["10000 per hour", "2000 per minute"]
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'bannedfromunturned@gmail.com'  # Replace with your email
+    MAIL_PASSWORD = 'your-email-password'  # Replace with your email password
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # Initialize Flask-Mail
+    mail = Mail(app)
+
+    # Initialize Flask-Notifications
+    notifications = Notifications(app)
