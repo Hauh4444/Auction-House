063e1e8a0ec76cb2c207aa7661385c22ec1e524c
Alex <escalatorcast@gmail.com>
2025-04-08 20:09:44 -0400

    Undid previous push

--- flask-server/app/main.py
+++ flask-server/app/main.py
@@ -3,20 +3,6 @@ import logging
 from flask.logging import default_handler
 
 app = create_app()
-app.logger.setLevel(logging.INFO)
-
-# Create a file handler
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.INFO)
-
-# Create a formatter and set it for the handler
-formatter = logging.Formatter(
-    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
-    )
-file_handler.setFormatter(formatter)
-
-# Add the handler to app.logger
-app.logger.addHandler(file_handler)
 
 @app.route("/test", methods=["GET"])
 def test():
